6a3955b8fcafce7a079fff0ea3384a236d41a109
Andrew Bishop <andrew.bishop53@gmail.com>
2019-01-02 17:36:29 -0800

    Updates

---
+++ precache-manifest.ef4147874b8249a85e33bab356b0b223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43e883092db3a8695b35",
+    "url": "/population-stats/static/css/main.7fbacd4b.chunk.css"
+  },
+  {
+    "revision": "43e883092db3a8695b35",
+    "url": "/population-stats/static/js/main.43e88309.chunk.js"
+  },
+  {
+    "revision": "19322cefe80f27ed772d",
+    "url": "/population-stats/static/js/1.19322cef.chunk.js"
+  },
+  {
+    "revision": "754592d57c56f8198dd8",
+    "url": "/population-stats/static/js/runtime~main.754592d5.js"
+  },
+  {
+    "revision": "afb68a5056e884ad4bcc0821975fee22",
+    "url": "/population-stats/index.html"
+  }
+];
